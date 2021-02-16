a4bd9dd71befcbcf393c665202f08d4665f9c27e
xausky <xausky@163.com>
2021-02-16 12:21:12 +0800

    update dep version

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,7 @@
 if [ $DRCD_ORIGIN ]; then
   sed -i "s|DRCD_ORIGIN|$DRCD_ORIGIN|g" ./build/web/index.html
 fi
+if [ $PRELOAD_SCRIPT ]; then
+  sed -i "s|</head>|$PRELOAD_SCRIPT</head>|g" ./build/web/index.html
+fi
 ./drcd
